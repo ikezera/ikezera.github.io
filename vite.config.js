57a71aeb4f0c1dd6e6fb25e6d25180ecf71028a9
Sergio Miguel <sergiobmiguel@msn.com>
2023-12-28 09:58:37 -0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import { resolve } from 'path';
 import posthtml from '@vituum/vite-plugin-posthtml';
 
 export default {
   plugins: [posthtml()],
-
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   server: {
     watch: {
+      include: ['./src/components/**', './src/styles/**'],
       usePolling: true,
     },
   },
